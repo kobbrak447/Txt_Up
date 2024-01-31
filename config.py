e28058edcc2991ff720889a731b8a795ec95c285
kobbrak447 <153929734+kobbrak447@users.noreply.github.com>
2024-01-31 10:48:16 +0530

    Update config.pyh

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6756823663:AAFVW3NDF0EB2UsTarqMxVqi0g5_ZjXXs3o")
-    API_ID = int(os.environ["API_ID", 27060147]
-    API_HASH = os.environ["API_HASH", "70f93c98c61a40f69c0f830bd4fece7c"]
-    AUTH_USERS = "6693536228,5684410709,1318247204,1511103739 """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6969961194:AAHg6UtYLwMavm0kUpLHJfOUDXmbRjbG8jQ")
+    API_ID = int(os.environ["API_ID", 23621134]
+    API_HASH = os.environ["API_HASH", "3e49039179441fb424d90680ecffe365"]
+    AUTH_USERS = "6960520819 """
 
